7dd09461315580b80927b4aff2f2e10f54e1b3dc
Alex Lo <alexislo15@hotmail.es>
2022-10-21 20:28:17 -0600

    Remove string

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
  * –––– Setup Apollo Client
  * –––––––––––––––––––––––––––––––––– */
 console.log("Apollo Client setup");
-const uri = process.env.GRAPHQL_ENDPOINT || "http://localhost:3000/api/graphql";
+const uri = process.env.GRAPHQL_ENDPOINT
 export const apolloClient = new ApolloClient({
   uri,
   cache: new InMemoryCache(),
