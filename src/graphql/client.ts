9954a4698bc3abdfecad6df6b407133f62944dd2
Alex Lo <alexislo15@hotmail.es>
2022-10-21 18:38:59 -0600

    Added production url

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.PRODUCTION_GRAPHQL_ENDPOINT || "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
